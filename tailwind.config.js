b7f794787e5e27d6b30562da56ed23017f9cc7e2
Gabriel <gdeoliveira.1999@gmail.com>
2021-03-17 19:18:28 -0400

    Project Base Config created

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        header: "#2A2A2A",
+      },
+      fontFamily: {
+        sans: ["Inter", "ui-sans-serif"],
+        raj: ["Rajdhani", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
