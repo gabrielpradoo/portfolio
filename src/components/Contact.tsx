bcf5736b5022e2d181b661b8cd98cdf8732e3694
Gabriel <gdeoliveira.1999@gmail.com>
2021-03-25 19:02:11 -0400

    Feat: Contact API created

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,4 +1,35 @@
 export function Contact() {
+  function sendEmail() {
+    const form = document.getElementById("form");
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const message = document.getElementById("message").value;
+
+      fetch("/api/email", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name,
+          email,
+          message,
+        }),
+      })
+        .then((response) => {
+          console.log(response);
+          alert("E-mail enviado com sucesso");
+        })
+        .catch((error) => {
+          console.log(error);
+          alert("Erro ao enviar e-mail");
+        });
+    });
+  }
+
   return (
     <div className="w-full bg-main">
       <div className=" font-raj max-w-screen-8xl mx-auto px-4 py-12 text-white flex flex-col items-center">
@@ -28,29 +59,37 @@ export function Contact() {
             gdeoliveira.1999@gmail.com
           </a>
         </p>
-        <form className="w-8/12 flex flex-col space-y-4 mt-6 max-midlle:w-9/12">
+        <form
+          id="form"
+          className="w-8/12 flex flex-col space-y-4 mt-6 max-midlle:w-9/12"
+        >
           <div
             className="flex justify-between space-x-4 
             max-midlle:flex-col max-midlle:space-x-0 max-midlle:space-y-4"
           >
             <input
               type="text"
+              id="name"
               placeholder="Name"
-              className=" w-6/12 py-2 px-4 rounded max-midlle:w-full"
+              className=" w-6/12 py-2 px-4 rounded max-midlle:w-full outline-none text-main font-bold"
             />
             <input
               type="text"
+              id="email"
               placeholder="E-mail address"
-              className=" w-6/12 py-2 px-4 rounded max-midlle:w-full"
+              className=" w-6/12 py-2 px-4 rounded max-midlle:w-full outline-none text-main font-bold"
             />
           </div>
           <textarea
             name=""
-            id=""
+            id="message"
             placeholder="Message"
-            className="py-2 px-4 h-64 rounded text-main font-medium"
+            className="py-2 px-4 h-64 rounded outline-none text-main text-lg font-medium"
           />
-          <button className="bg-green-500 inline-block w-4/12 px-6 py-3 rounded max-md:w-5/12 max-sm:w-full font-semibold text-xl">
+          <button
+            onClick={sendEmail}
+            className="bg-green-500 inline-block w-4/12 px-6 py-3 rounded max-md:w-5/12 max-sm:w-full font-semibold text-xl"
+          >
             Submit
           </button>
         </form>
--- src/pages/api/Email.tsx
+++ src/pages/api/Email.tsx
@@ -1,4 +1,37 @@
 import nodemailer from "nodemailer";
 import { NextApiRequest, NextApiResponse } from "next";
 
-export default function sendEmail(req: NextApiRequest, res: NextApiResponse) {}
+import config from "../../../smtp";
+
+export default async function sendEmail(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const transporter = nodemailer.createTransport({
+    host: config.host,
+    port: config.port,
+    secure: false,
+    auth: {
+      user: config.user,
+      pass: config.pass,
+    },
+    tls: {
+      rejectUnauthorized: false,
+    },
+  });
+
+  transporter
+    .sendMail({
+      from: `"GABRIEL DEV PORTFOLIO 🖥️" <${config.user}>`,
+      to: "gdeoliveira.1999@gmail.com, g.prado0102@gmail.com",
+      replyTo: req.body.email,
+      subject: "CONTATO FEITO PELO PORTFOLIO",
+      text: req.body.message,
+    })
+    .then((response) => {
+      res.send(response);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+}
